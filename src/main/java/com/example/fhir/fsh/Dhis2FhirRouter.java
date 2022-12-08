36cc969ea5fb757433d0669a4c81aa7a8cfa27be
Morten Hansen <morten@winterop.com>
2022-12-08 10:10:23 +0700

    chore: update spring boot, hawtio, adds license

--- src/main/java/com/example/fhir/fsh/Dhis2FhirRouter.java
+++ src/main/java/com/example/fhir/fsh/Dhis2FhirRouter.java
@@ -1,7 +1,36 @@
+/*
+ * Copyright (c) 2004-2022, University of Oslo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * Neither the name of the HISP project nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package com.example.fhir.fsh;
 
-import com.example.fhir.fsh.domain.dhis2.OptionSets;
+import java.net.URI;
+
 import lombok.AllArgsConstructor;
+
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -9,7 +38,7 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.net.URI;
+import com.example.fhir.fsh.domain.dhis2.OptionSets;
 
 @Component
 @AllArgsConstructor
@@ -20,7 +49,8 @@ public class Dhis2FhirRouter extends RouteBuilder
     private final RestTemplate restTemplate;
 
     @Override
-    public void configure() throws Exception
+    public void configure()
+        throws Exception
     {
         UriComponents uriComponents = UriComponentsBuilder.newInstance()
             .uri( URI.create( dhis2Properties.getBaseUrl() ) )
@@ -35,7 +65,7 @@ public class Dhis2FhirRouter extends RouteBuilder
 
         from( "timer:foo?repeatCount=1" )
             .process( x -> x.getIn().setBody( response.getBody() ) )
-            .process("dhis2Cleanup")
+            .process( "dhis2Cleanup" )
             .to( "mustache:fsh/fsh-optionSets.mustache" )
             .to( "file:output?fileName=optionSets.fsh" );
     }
